98533af5f788a0a16d6b85ab87c9d4f868eb843e
Chrisctr <credfern96@gmail.com>
2020-03-26 14:05:30 +0000

    Added java files for exercise 1 and 2

---
+++ src/iteration/trial/exerc1/Iterator.java
@@ -0,0 +1,16 @@
+package iteration.trial.exerc1;
+
+public class Iterator {
+
+	public String flowChart(int A, int max) {
+
+		while (A <= max) {
+			System.out.println("'A' number: " + A);
+			A++;
+		}
+
+		return ("END");
+
+	}
+
+}
---
+++ src/iteration/trial/exerc2/Runner.java
@@ -0,0 +1,12 @@
+package iteration.trial.exerc2;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+		Iterator flowchart = new Iterator();
+		System.out.println(flowchart.flowChart(100, 200)); /// 1st arg is start, 2nd is end
+
+	}
+
+}
